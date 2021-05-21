8bd4c7df5d29c2d51c661301235e71e534db8981
xmie <mickael.eriksson@scott-tiger.dk>
2021-05-21 08:01:58 +0200

    Add cors

--- src/main/java/com/mickenet/mongotest/MongoTestApplication.java
+++ src/main/java/com/mickenet/mongotest/MongoTestApplication.java
@@ -2,6 +2,9 @@ package com.mickenet.mongotest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -11,5 +14,13 @@ public class MongoTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(MongoTestApplication.class, args);
     }
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/registration").allowedOrigins("http://localhost:8080");
+            }
+        };
+    }
 }
--- src/main/java/com/mickenet/mongotest/ViewAnmalningarController.java
+++ src/main/java/com/mickenet/mongotest/ViewAnmalningarController.java
@@ -15,28 +15,31 @@ public class ViewAnmalningarController {
     @Autowired
     private RegistrationService registrationService;
 
+    @CrossOrigin
     @GetMapping(value = "/registration/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Registration> getRegistration(@PathVariable(value = "id") UUID id){
         return new ResponseEntity<>(registrationService.getReregistrationById(id), HttpStatus.OK);
     }
-
+    @CrossOrigin
     @PostMapping(value = "/registration")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Registration> createRegistration(@RequestBody Registration registration){
         return new ResponseEntity<>(registrationService.createRegistration(registration), HttpStatus.CREATED);
     }
-
+    @CrossOrigin
     @PutMapping(value = "/registration/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Registration> updateRegistration(@RequestBody Registration registration, @PathVariable(value = "id") UUID id){
         return new ResponseEntity<Registration>(registrationService.updateRegistration(registration), HttpStatus.OK);
     }
+    @CrossOrigin
     @GetMapping(value = "/registration/")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<Registration>> getRegistrations(){
         return new ResponseEntity<List<Registration>>(registrationService.getReregistration(), HttpStatus.OK);
     }
+    @CrossOrigin
     @DeleteMapping(value = "/registration/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Registration> deleteRegistration(@PathVariable(value="id") UUID id){
